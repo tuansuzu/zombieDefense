7da97bab68be51cd2aa7deefcf759c8b841b9186
tuansuzu <mr.buiminhtuan@yahoo.com>
2018-08-25 13:56:30 +0700

    Add files via upload

---
+++ MasterController/AdController/AdController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdController
+{
+    private static float lastAdTime = float.MinValue;
+
+    public static void CheckAndShowAd()
+    {
+        if (Time.time - lastAdTime < 90) return;
+        bool isRemoveAd = PlayerPrefs.GetInt("isRemoveAd", 0) == 0 ? false : true;
+        if (isRemoveAd) return;
+
+#if UNITY_ANDROID || UNITY_IPHONE
+        bool result = AdmobController.instance.ShowInterstitial();
+        if (result == false) AdmobController.instance.RequestInterstitial();
+#endif
+        lastAdTime = Time.time;
+    }
+
+    public static void ShowBanner()
+    {
+        bool isRemoveAd = PlayerPrefs.GetInt("isRemoveAd", 0) == 0 ? false : true;
+        if (isRemoveAd) return;
+
+#if UNITY_ANDROID || UNITY_IPHONE
+        AdmobController.instance.ShowBanner();
+#endif
+    }
+
+    public static void HideBanner()
+    {
+#if UNITY_ANDROID || UNITY_IPHONE
+        AdmobController.instance.HideBanner();
+#endif
+    }
+}
