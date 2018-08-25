bba6d36ae587ffeb84ba52ba7eebbee464fd93fb
tuansuzu <mr.buiminhtuan@yahoo.com>
2018-08-25 13:59:54 +0700

    Add files via upload

---
+++ LevelController/LevelController.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#pragma warning disable 0219
+#pragma warning disable 0414
+public class LevelController : MonoBehaviour
+{
+
+    // Use this for initialization
+    [HideInInspector]
+    public LevelDataController.LevelData currentLevelData;
+    [HideInInspector]
+    public int totalWaves;
+    [HideInInspector]
+    public int currentWave;
+    [HideInInspector]
+    public int totalSequences;
+    [HideInInspector]
+    public int totalSequenceIndex;
+    [HideInInspector]
+    public bool isLastWave;
+    [HideInInspector]
+    public bool isLastSequence;
+    bool isCheckingEnemyExistToStartNewWave;
+    void Start()
+    {
+        if (Master.Level == null)
+        {
+            Master.Level = this;
+        }
+        SetLevelInfo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDestroy()
+    {
+        StopAllCoroutines();
+        CancelInvoke();
+    }
+
+    public void SetLevelInfo()
+    {
+        int currentLevel = Master.LevelData.currentLevel;
+        currentLevelData = Master.LevelData.GetLevelDataByIndex(Master.LevelData.currentLevel);
+        totalWaves = currentLevelData.ListWaves.Count;
+        totalSequences = 0;
+        //get total sequence
+        for (int i = 0; i < currentLevelData.ListWaves.Count; i++)
+        {
+            totalSequences += currentLevelData.ListWaves[i].ListSequences.Count;
+        }
+
+        totalSequenceIndex = 0;
+        Master.Lane.SetLane(currentLevelData.NumberOfLanes, currentLevelData.NumberOfPositionsCanBuildUnitInLane);
+        Master.Gameplay.gold = currentLevelData.InitialGold;
+    }
+
+    public void StartInitEnenmy()
+    {
+        InitEnemy(0, 0);
+    }
+
+    private void InitEnemy(int waveIndex, int sequenceIndexOfWave)
+    {
+        if (waveIndex >= currentLevelData.ListWaves.Count) return;
+
+        LevelDataController.Waves wave = currentLevelData.ListWaves[waveIndex];
+
+        if (sequenceIndexOfWave >= wave.ListSequences.Count) return;
+
+        LevelDataController.Sequences sequence = wave.ListSequences[sequenceIndexOfWave];
+
+        isCheckingEnemyExistToStartNewWave = false;
+
+        Master.WaitAndDo(sequence.Time, () =>
+        {
+            totalSequenceIndex++;
+            foreach (LevelDataController.EnemyAtSequence enemytAtSequence in sequence.ListEnemyAtSequence)
+            {
+                GameObject pf_enemy = Master.GetEnemyPrefabByID(enemytAtSequence.EnemyID);
+                GameObject go_enemy = NGUITools.AddChild(Master.Gameplay.enemiesRoot, pf_enemy);
+                go_enemy.transform.localPosition = new Vector3(Master.Gameplay.outOfScreenPos[2].localPosition.x, Master.Lane.positionOfLane[enemytAtSequence.Lane].localPosition.y - 30, 0);
+                go_enemy.GetComponent<EnemyController>().status.CurrentLane = enemytAtSequence.Lane;
+                Master.Lane.SetCharacterAtLane(go_enemy, enemytAtSequence.Lane);
+            }
+            sequenceIndexOfWave++;
+            if (sequenceIndexOfWave >= wave.ListSequences.Count)
+            {
+                if (!isCheckingEnemyExistToStartNewWave)
+                {
+                    CheckEnemyExistAndStartNewWave(waveIndex, sequenceIndexOfWave);
+                }
+            }
+            else
+            {
+                InitEnemy(waveIndex, sequenceIndexOfWave);
+            }
+        }, this);
+    }
+
+    void CheckEnemyExistAndStartNewWave(int waveIndex, int sequenceIndex)
+    {
+        isCheckingEnemyExistToStartNewWave = true;
+        Master.WaitAndDo(0.5f, () =>
+        {
+            bool isExistEnemy = Master.Lane.isExistCharacterByTagInAllLane("Enemy");
+            if (!isExistEnemy)
+            {
+                sequenceIndex = 0;
+                waveIndex++;
+                InitEnemy(waveIndex, sequenceIndex);
+            }
+            else
+            {
+                CheckEnemyExistAndStartNewWave(waveIndex, sequenceIndex);
+            }
+        }, this);
+    }
+
+}
---
+++ LevelController/LevelDataController.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using CodeStage.AntiCheat.ObscuredTypes;
+
+public class LevelDataController : MonoBehaviour
+{
+
+    // Use this for initialization
+    //Unit data
+    [XmlRoot("LevelDataCollection")]
+    public class LevelDataCollection
+    {
+        [XmlArray("Levels")]
+        [XmlArrayItem("Level")]
+        public List<LevelData> ListLevelData = new List<LevelData>();
+    }
+    public LevelDataCollection levelDataCollection;
+
+    [System.Serializable]
+    public class LevelData
+    {
+        public int LevelIndex = 0;
+        public int NumberOfLanes;
+        public int NumberOfPositionsCanBuildUnitInLane;
+        public int NumberOfUnitsAllowedDead;
+        public int InitialGold;
+        [XmlArray("Waves")]
+        [XmlArrayItem("Wave")]
+        public List<Waves> ListWaves = new List<Waves>();
+    }
+
+    [System.Serializable]
+    public class Waves
+    {
+        //public int WaveIndex;
+        [XmlArray("Sequences")]
+        [XmlArrayItem("Sequence")]
+        public List<Sequences> ListSequences = new List<Sequences>();
+    }
+
+    [System.Serializable]
+    public class Sequences
+    {
+        public float Time;
+        [XmlArray("Enemies")]
+        [XmlArrayItem("Enemy")]
+        public List<EnemyAtSequence> ListEnemyAtSequence = new List<EnemyAtSequence>();
+    }
+
+
+    [System.Serializable]
+    public class EnemyAtSequence
+    {
+        public string EnemyID;
+        public int Lane;
+    }
+
+    private bool startInitEnemy;
+
+    public int totalLevel = 40;
+    public int lastLevel = 1;
+    [HideInInspector]
+    public int currentLevel = 1;
+    public Dictionary<int, int> starAtLevel = new Dictionary<int, int>();
+
+    void Awake()
+    {
+        if (Master.LevelData == null)
+        {
+            Master.LevelData = this;
+        }
+
+        LoadLevelData();
+    }
+
+    void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+
+    public void LoadLevelData()
+    {
+        TextAsset textAsset = Resources.Load("Data/Levels/LevelData") as TextAsset;
+        var serializer = new System.Xml.Serialization.XmlSerializer(typeof(LevelDataCollection));
+        using (var reader = new System.IO.StringReader(textAsset.text))
+        {
+            this.levelDataCollection = (LevelDataCollection)serializer.Deserialize(reader);
+        }
+
+        for (int i = 0; i < levelDataCollection.ListLevelData.Count; i++)
+        {
+            levelDataCollection.ListLevelData[i].LevelIndex = i + 1;
+        }
+
+        lastLevel = ObscuredPrefs.GetInt("LastLevel", 0);
+        totalLevel = levelDataCollection.ListLevelData.Count;
+        // lastLevel = 2;
+    }
+
+    public LevelData GetLevelDataByIndex(int levelIndex)
+    {
+
+        //foreach (LevelData levelData in levelDataCollection.ListLevelData)
+        // {
+        //   if (levelIndex == levelData.LevelIndex) return levelData;
+        // }
+
+        return levelDataCollection.ListLevelData[levelIndex - 1];
+    }
+
+    public int GetStarAtLevel(int level)
+    {
+        return ObscuredPrefs.GetInt("StarAtLevel_" + level, 0);
+    }
+
+    public void SetStarAtLevel(int level, int star)
+    {
+        ObscuredPrefs.SetInt("StarAtLevel_" + level, star);
+        ObscuredPrefs.Save();
+    }
+
+    public void SetLastLevel(int level)
+    {
+        if (level > lastLevel)
+        {
+            lastLevel = level;
+            ObscuredPrefs.SetInt("LastLevel", level);
+            ObscuredPrefs.Save();
+            Master.QuestData.SetProgressValue("02", lastLevel);
+        }
+    }
+
+    public int GetTotalStarAtLevelsGot()
+    {
+        int totalStarAtLevels = 0;
+        for(int i = 1; i <= lastLevel; i++)
+        {
+            totalStarAtLevels += GetStarAtLevel(i);
+        }
+        return totalStarAtLevels;
+    }
+
+    public void Save(LevelDataCollection levelDataCollection)
+    {
+        string path = Application.dataPath + "/Resources/Data/Levels/LevelData.xml";
+
+        var serializer = new XmlSerializer(typeof(LevelDataCollection));
+        using (var stream = new FileStream(path, FileMode.Create))
+        {
+
+            serializer.Serialize(stream, levelDataCollection);
+            Debug.Log("Saved XML to " + path);
+        }
+    }
+
+
+    //public bool CheckGotGemRewardAtLevelAndStarGot(int level, int starGotAtLevel)
+    //{
+    //    bool isGotReward = (ObscuredPrefs.GetInt("IsGotGemRewardAtLevelAndStar_" + level + "_" + starGotAtLevel, 0) == 0) ? false : true;
+    //    return isGotReward;
+    //}
+
+    //public void SetGotGemRewardAtLevelAndStarGot(int level, int starGotAtLevel)
+    //{
+    //    ObscuredPrefs.SetInt("IsGotGemRewardAtLevelAndStar_" + level + "_" + starGotAtLevel, 1);
+    //    ObscuredPrefs.Save();
+    //}
+
+}
