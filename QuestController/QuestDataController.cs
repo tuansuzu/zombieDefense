943aaef2262b06b963489eb12cc138c498c9f125
tuansuzu <mr.buiminhtuan@yahoo.com>
2018-08-25 14:02:41 +0700

    Add files via upload

---
+++ QuestController/QuestDataController.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+using System.IO;
+using System.Collections.Generic;
+using CodeStage.AntiCheat.ObscuredTypes;
+
+public class QuestDataController : MonoBehaviour
+{
+
+    //Unit data
+    [XmlRoot("QuestDataCollection")]
+    public class QuestDataCollection
+    {
+        [XmlArray("Quests")]
+        [XmlArrayItem("Quest")]
+        public List<QuestData> ListQuestData = new List<QuestData>();
+    }
+    public QuestDataCollection questDataCollection;
+
+    [System.Serializable]
+    public class QuestData
+    {
+        public string QuestID = "";
+        public string QuestName = "";
+        public RequireValue RequireValue = new RequireValue();
+        public Reward Reward = new Reward();
+        public int CurrentProgressValue = 0;
+        public int CurrentStep = 0;
+    }
+    [System.Serializable]
+    public class RequireValue
+    {
+        public int Value = 0;
+        public float PercentIncreasePerStep = 0;//%
+    }
+    [System.Serializable]
+    public class Reward
+    {
+        public int Star = 0;
+        public int Gem = 0;
+        public float PercentIncreasePerStep = 0; //%
+    }
+
+    void Awake()
+    {
+        if (Master.QuestData == null)
+        {
+            Master.QuestData = this;
+        }
+        LoadQuestData();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Master.QuestData.SetProgressValue("06", Master.LevelData.GetTotalStarAtLevelsGot());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadQuestData()
+    {
+        TextAsset textAsset = Resources.Load("Data/Quests/QuestData") as TextAsset;
+        var serializer = new System.Xml.Serialization.XmlSerializer(typeof(QuestDataCollection));
+        using (var reader = new System.IO.StringReader(textAsset.text))
+        {
+            this.questDataCollection = (QuestDataCollection)serializer.Deserialize(reader);
+        }
+
+        //set value progress and step
+        for (int i = 0; i < questDataCollection.ListQuestData.Count; i++)
+        {
+            string questID = questDataCollection.ListQuestData[i].QuestID;
+            int requireValue = questDataCollection.ListQuestData[i].RequireValue.Value;
+            int currentValue = GetCurrentProgressValue(questID);
+            int currentStep = GetCurrentStep(questID);
+            float increasePercentRequireValue = questDataCollection.ListQuestData[i].RequireValue.PercentIncreasePerStep;
+            questDataCollection.ListQuestData[i].RequireValue.Value = (int)Master.IncreaseValues(requireValue, currentStep, increasePercentRequireValue);
+
+            int star = questDataCollection.ListQuestData[i].Reward.Star;
+            int gem = questDataCollection.ListQuestData[i].Reward.Gem;
+            float increasePercentReward = questDataCollection.ListQuestData[i].Reward.PercentIncreasePerStep;
+            questDataCollection.ListQuestData[i].Reward.Star = (int)Master.IncreaseValues(star, currentStep, increasePercentReward);
+            questDataCollection.ListQuestData[i].Reward.Gem = (int)Master.IncreaseValues(gem, currentStep, increasePercentReward);
+            questDataCollection.ListQuestData[i].CurrentStep = currentStep;
+            questDataCollection.ListQuestData[i].CurrentProgressValue = currentValue;
+        }
+    }
+
+    public QuestData GetQuestDataByID(string id)
+    {
+        foreach (QuestData questData in questDataCollection.ListQuestData)
+        {
+            if (id == questData.QuestID)
+            {
+                return questData;
+            }
+        }
+        return null;
+    }
+
+    public void IncreaseProgressValue(string questID, int value = 0, bool isAllowLargerThanRequireValue = true)
+    {
+        int currentProgressValue = GetCurrentProgressValue(questID);
+        if (!isAllowLargerThanRequireValue && currentProgressValue >= GetQuestDataByID(questID).RequireValue.Value)
+        {
+            return;
+        }
+
+        if (value > 0)
+        {
+            currentProgressValue = currentProgressValue + value;
+        }
+        else
+        {
+            currentProgressValue = currentProgressValue + 1;
+        }
+        ObscuredPrefs.SetInt("CurrentProgressValueQuest_" + questID, currentProgressValue);
+        ObscuredPrefs.Save();
+        //LoadQuestData();
+    }
+
+    public void SetProgressValue(string questID, int value)
+    {
+        ObscuredPrefs.SetInt("CurrentProgressValueQuest_" + questID, value);
+        ObscuredPrefs.Save();
+    }
+
+    public void IncreaseStep(string questID)
+    {
+        int currentStep = GetCurrentStep(questID);
+        currentStep = currentStep + 1;
+        ObscuredPrefs.SetInt("CurrentStepQuest_" + questID, currentStep);
+        ObscuredPrefs.Save();
+        // LoadQuestData();
+    }
+
+    public void SetStep(string questID, int value)
+    {
+        ObscuredPrefs.SetInt("CurrentStepQuest_" + questID, value);
+        ObscuredPrefs.Save();
+    }
+
+    public int GetCurrentProgressValue(string questID)
+    {
+        return ObscuredPrefs.GetInt("CurrentProgressValueQuest_" + questID, 0);
+    }
+
+    public int GetCurrentStep(string questID)
+    {
+        return ObscuredPrefs.GetInt("CurrentStepQuest_" + questID, 0);
+    }
+
+    public bool isHaveQuestComplete()
+    {
+        Master.QuestData.LoadQuestData();
+        foreach (QuestData item in questDataCollection.ListQuestData)
+        {
+            if (item.CurrentProgressValue >= item.RequireValue.Value)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public void Save(QuestDataCollection questDataCollection)
+    {
+        string path = Application.dataPath + "/Resources/Data/Quests/QuestData.xml";
+
+        var serializer = new XmlSerializer(typeof(QuestDataCollection));
+        using (var stream = new FileStream(path, FileMode.Create))
+        {
+            serializer.Serialize(stream, questDataCollection);
+            Debug.Log("Saved XML to " + path);
+        }
+    }
+}
---
+++ QuestController/QuestItemController.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestItemController : MonoBehaviour
+{
+
+    // Use this for initialization
+    QuestDataController.QuestData questData;
+    UITexture bgTexture;
+    UITexture iconTexture;
+    UILabel questNameLabel;
+    UISlider progressSlider;
+    UILabel progressValueLabel;
+    UILabel starValueLabel;
+    UILabel gemValueLabel;
+    public bool isComplete;
+
+    void Awake()
+    {
+        AssignObject();
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void AssignObject()
+    {
+        bgTexture = Master.GetChildByName(gameObject, "BG").GetComponent<UITexture>();
+        iconTexture = Master.GetChildByName(gameObject, "Icon").GetComponent<UITexture>();
+        questNameLabel = Master.GetChildByName(gameObject, "QuestName").GetComponent<UILabel>();
+        starValueLabel = Master.GetChildByName(gameObject, "StarValueLabel").GetComponent<UILabel>();
+        gemValueLabel = Master.GetChildByName(gameObject, "GemValueLabel").GetComponent<UILabel>();
+        progressSlider = Master.GetChildByName(gameObject, "ProgressBar").GetComponent<UISlider>();
+        progressValueLabel = Master.GetChildByName(gameObject, "ProgressValue").GetComponent<UILabel>();
+    }
+
+    public void SetAttribute(QuestDataController.QuestData questDataGet)
+    {
+
+        this.questData = questDataGet;
+
+        if (questData.CurrentProgressValue >= questData.RequireValue.Value)
+        {
+            isComplete = true;
+        }
+        else
+        {
+            isComplete = false;
+        }
+
+        iconTexture.mainTexture = Resources.Load<Texture2D>("Textures/Quests/quest_icon_" + questData.QuestID);
+        questNameLabel.text = questData.QuestName;
+        progressSlider.value = (float)questData.CurrentProgressValue / questData.RequireValue.Value;
+        progressValueLabel.text = questData.CurrentProgressValue + "/" + questData.RequireValue.Value;
+        starValueLabel.text = questData.Reward.Star.ToString();
+        gemValueLabel.text = questData.Reward.Gem.ToString();
+
+        SetButton();
+    }
+
+    void SetButton()
+    {
+        if (isComplete)
+        {
+            bgTexture.color = new Color((float)60 / 255, (float)115 / 255, (float)158 / 255, 1);
+            bgTexture.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            GetComponent<BoxCollider2D>().enabled = true;
+        }
+        else
+        {
+            bgTexture.color = new Color((float)60 / 255, (float)60 / 255, (float)60 / 255, 1);
+            bgTexture.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    public void OnTouchIn()
+    {
+        if (!isComplete) return;
+
+        Master.Stats.Gem += questData.Reward.Gem;
+        Master.Stats.Star += questData.Reward.Star;
+        Master.QuestData.IncreaseStep(questData.QuestID);
+        Master.QuestData.LoadQuestData();
+        //SetAttribute(Master.QuestData.GetQuestDataByID(questData.QuestID));
+        Master.UI.ShowDialog("GotRewardDialog", 0.5f, new string[] { questData.Reward.Gem.ToString(), questData.Reward.Star.ToString() });
+        Master.UIMenu.panels[3].GetComponent<QuestPanelController>().SetListQuest();
+
+    }
+
+}
